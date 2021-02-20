4873890e4943487bc78b19dffa7e261d1f7eb389
LaiaFC <laiafc@proximus.be>
2021-02-20 19:16:24 +0100

    up to point 6

--- Mehrnoosh_Rproject.R
+++ Mehrnoosh_Rproject.R
@@ -1,3 +1,8 @@
+#R PROJECT FOR DR OGHBAIE
+
+#Author: Laia Fernández Calvo
+
+
 #Read in file
 proteinGroups <- read.delim("~/Documents/GitHub/Mehrnoosh_Rproject/proteinGroups.txt")
 
@@ -31,17 +36,58 @@ t163 <- subset(cleandata, select = c(Protein.IDs, Gene.names, grep("LFQ.intensit
 
 #5. Log2 transform LFQ intensity.
 
+#I decided to add +1 to every value to avoid -inf when going log2(0). As the minimum non-zero value of the
+#LFQ intensities is 211600 adding +1 will not greatly affect the log2.
+
 logt144 <- t144
-logt144[, 3:14] <- log2(t144[3:14])
+logt144[, 3:14] <- log2(t144[3:14]+1)
 
 logt159 <- t159
-logt159[, 3:14] <- log2(t159[3:11])
+logt159[, 3:14] <- log2(t159[3:11]+1)
 
 logt163 <- t163
-logt163[, 3:14] <- log2(t163[3:11])
+logt163[, 3:14] <- log2(t163[3:11]+1)
+
+
+
+#chech the minimum non-zero of every numerical column
+nonzeromin <- function(data){
+  for(i in 1:ncol(data)){
+    col = data[,i]
+    print(min(subset(col, col>0)))
+  }
+}
+
+todomin <- subset(cleandata, select = grep("LFQ.intensity", names(cleandata)))
+nonzeromin(todomin)
 
 
 #6. Calculate the average log2 LFQ intensities in each condition.
+
+#for tissue 144
+logt144$mean10 <- rowMeans(logt144[,c(3,4,5)])
+logt144$mean2 <- rowMeans(logt144[,c(6,7,8)])
+logt144$mean1 <- rowMeans(logt144[,c(9,10,11)])
+logt144$mean9 <- rowMeans(logt144[,c(12,13,14)])
+
+#for tissue 159
+logt159$mean5 <- rowMeans(logt159[,c(3,4,5)])
+logt159$mean4 <- rowMeans(logt159[,c(6,7,8)])
+logt159$mean3 <- rowMeans(logt159[,c(9,10,11)])
+
+#There's 5a.1 etc WHAT TO DO?
+
+
+#for tissue 163
+
+logt163$mean8 <- rowMeans(logt163[,c(3,4,5)])
+logt163$mean7 <- rowMeans(logt163[,c(6,7,8)])
+logt163$mean6 <- rowMeans(logt163[,c(9,10,11)])
+
+
+#7. Calculate log2foldchange by subtracting the avg log2 LFQ intensity of control from case.
+
+#Don't know what to substract
 
 
 View(proteinGroups$Intensity)
@@ -49,14 +95,10 @@ View(proteinGroups$Intensity.144T_IgG_10a)
 
 
 
-#7. Calculate log2foldchange by subtracting the avg log2 LFQ intensity of control from case.
-
-
-
-
-
 #8. Perform t.test() between every records in case and control and extract the p.value from it.
 #You can use try() to continue your calculation whenever not enough replicates exist.
+
+
 
 
 
