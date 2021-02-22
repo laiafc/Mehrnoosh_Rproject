1267601bc2f4dc5dd10522e6deab4a1acf04f94c
LaiaFC <laiafc@proximus.be>
2021-02-22 15:55:27 +0100

    finished?

--- Mehrnoosh_Rproject.R
+++ Mehrnoosh_Rproject.R
@@ -3,13 +3,18 @@
 #Author: Laia Fernández Calvo
 
 library(ggplot2)
+library(ggrepel)
+
 
 #Read in file
 proteinGroups <- read.delim("~/Documents/GitHub/Mehrnoosh_Rproject/proteinGroups.txt")
 
 names(proteinGroups)
 
+
+####
 #3.Remove every record that seem to be a Potential Contaminant or Reverse sequence from the table.
+####
 
 levels(proteinGroups$Reverse)
 levels(proteinGroups$Potential.contaminant)
@@ -19,6 +24,7 @@ cleandata <- subset(proteinGroups, Reverse != "+")
 cleandata <- subset(proteinGroups, Potential.contaminant != "+")
 
 
+####
 #4. Separate different tissue LFQ intensity columns. Don’t forget to include gene.name and protein IDs.
 #There are three different tissues [144/159/163], they could be either tumor or normal [T/N].
 #The name format is like [144/159/163] [T/N]_[ORF1/IgG]_[1-10][a/b/c].
@@ -26,8 +32,7 @@ cleandata <- subset(proteinGroups, Potential.contaminant != "+")
 #The number following by the antibody refer to specific condition (combination of tissue and antibody).
 #In total we have 10 conditions here. The last lower alphabet is the number of replicates.
 #A comparison could be either (159T_ORF1 against 159T_IgG) or (159T_ORF1 against 159N_ORF1).
-
-#Ask me questions if you have problem figuring the experiment design.
+####
 
 
 t144 <- subset(cleandata, select = c(Protein.IDs, Gene.names, grep("LFQ.intensity.144", names(cleandata))))
@@ -35,7 +40,9 @@ t159 <- subset(cleandata, select = c(Protein.IDs, Gene.names, grep("LFQ.intensit
 t163 <- subset(cleandata, select = c(Protein.IDs, Gene.names, grep("LFQ.intensity.163", names(cleandata))))
 
 
+####
 #5. Log2 transform LFQ intensity.
+####
 
 #"Zero values are missing ( the intensity doesn’t reach a threshold ) or they just didn’t exist." 
 
@@ -48,8 +55,6 @@ t163[t163 == 0] <- NA
 #"If only one replicate is non-zero or all replicates were zero, it didn’t exist."
 
 #if within a condition, only one replicate is not NA - convert that to NA
-
-
 
 changetoNA <- function(x){
   xt=t(x)
@@ -68,7 +73,6 @@ changetoNA <- function(x){
 }
 
 
-
 t144[3:5] <- changetoNA(t144[3:5])
 t144[6:8] <- changetoNA(t144[6:8])
 t144[9:11] <- changetoNA(t144[9:11])
@@ -85,11 +89,6 @@ t163[6:8] <- changetoNA(t163[6:8])
 t163[9:11] <- changetoNA(t163[9:11])
 
 
-
-
-#I decided to add +1 to every value to avoid -inf when going log2(0). As the minimum non-zero value of the
-#LFQ intensities is 211600 adding +1 will not greatly affect the log2.
-
 logt144 <- t144
 logt144[, 3:14] <- log2(t144[3:14])
 
@@ -101,38 +100,9 @@ logt163[, 3:11] <- log2(t163[3:11])
 
 
 
-# #chech the minimum non-zero of every numerical column
-# nonzeromin <- function(data){
-#   for(i in 1:ncol(data)){
-#     col = data[,i]
-#     print(min(subset(col, col>0)))
-#   }
-# }
-# 
-# 
-# #check the minimun non-zero of the whole dataset
-# nonzerominwhole <- function(data){
-#   if(is.data.frame(data)){
-#     mins = vector(lenght=ncol(data))
-#     for(i in 1:ncol(data)){
-#       col=data[,i]
-#       mins[i] = min(subset(col, col>0))
-#     }
-#     print(min(mins))
-#   }
-#   else
-#     if(is.vector(data)){
-#       print(min(subset(data, data>0)))
-#     }
-# }
-# 
-# 
-# todomin <- subset(cleandata, select = grep("LFQ.intensity", names(cleandata)))
-# nonzeromin(todomin)
-
-
-
+####
 #6. Calculate the average log2 LFQ intensities in each condition.
+####
 
 #for tissue 144
 logt144$mean10 <- rowMeans(logt144[,c(3,4,5)], na.rm = TRUE)
@@ -153,8 +123,9 @@ logt163$mean6 <- rowMeans(logt163[,c(9,10,11)], na.rm = TRUE)
 
 
 
-
+####
 #7. Calculate log2foldchange by subtracting the avg log2 LFQ intensity of control from case.
+####
 
 #To compare:
 # 163T_ORF1 vs. 163N_ORF1
@@ -180,21 +151,10 @@ logt163$TvsN<- logt163$mean6 - logt163$mean8
 logt163$ORFvsIgG<- logt163$mean6 - logt163$mean7
 
 
-
+####
 #8. Perform t.test() between every records in case and control and extract the p.value from it.
 #You can use try() to continue your calculation whenever not enough replicates exist.
-
-# ttest <- function(x,y){
-#   p_values=vector()
-#   for(i in 1:nrow(x)){
-#     try(
-#       #p_val[1,i] <- t.test(t(x[i,]),t(y[i,]), paired = TRUE)$p.value
-#       p_values[i] <- t.test(t(x[i,]),t(y[i,]), paired = TRUE)$p.value
-#     )
-#   }
-#   return(p_values)
-# }
-
+####
 
 ttest <- function(x,y){
   p_values=vector()
@@ -212,111 +172,121 @@ ttest <- function(x,y){
 
 
 
-
-
 #for tissue 144
 # 144T_ORF1 vs. 144T_IgG
-
 logt144$p_val9_10 <- ttest(logt144[,3:5], logt144[,12:14])
 logt144$p_val1_2 <-ttest(logt144[,6:8], logt144[,9:11])
 
 
 #for tissue 159
-
 logt159$p_valT_N <- ttest(logt159[,3:5], logt159[,12:14])
 logt159$p_valORF_IgG <- ttest(logt159[,3:5], logt159[,6:8])
 
 #for tissue 163
-
 logt163$p_valT_N <- ttest(logt163[,3:5], logt163[,12:14])
 logt163$p_valORF_IgG <- ttest(logt163[,6:8], logt163[,12:14])
 
 
+####
 #9. Calculate adjusted p.value from the p.value distribution.
-
+####
 
 #for tissue 144
 # 144T_ORF1 vs. 144T_IgG
-
 logt144$p_adjust_9_10 <- p.adjust(logt144$p_val9_10 , method = "BH")
 logt144$p_adjust_1_2 <- p.adjust(logt144$p_val1_2 , method = "BH")
 
 
 #for tissue 159
-
 logt159$p_adjust_T_N <- p.adjust(logt159$p_valT_N , method = "BH")
 logt159$p_adjust_ORF_IgG <- p.adjust(logt159$p_valORF_IgG , method = "BH")
 
 
 #for tissue 163
-
 logt163$p_adjust_T_N <- p.adjust(logt163$p_valT_N , method = "BH")
 logt163$p_adjust_ORF_IgG <- p.adjust(logt163$p_valORF_IgG , method = "BH")
 
 
 
+####
 #10. Draw the volcano plot using -log10(adjusted p.value) on the y-axis and log2fold change on the x-axis.
+####
+
+#volcano plot function
+
+#Decided to choose significant values with p.value < 0.05 and to put threshold for the fold change at
+#-1/1 log2fold change. These could be changed if desired.
 
 
-volplot <- function(dataframe, x, y, title){
-  ggplot(data=dataframe, aes(x=x, y=-log10(y))) +
+volplot <- function(dataframe, x, y, title, label){
+  ggplot(data=dataframe, aes(x=x, y=-log10(y), col= label)) +
     theme_bw()+
     geom_point(size = 1) + 
     geom_vline(xintercept=c(-1, 1), col="blue") +
     geom_hline(yintercept=-log10(0.05), col="green")+
-    ylim(-1,4)+
-    xlim(-7,10)+
+    #ylim(-1,4)+
+    #xlim(-7,10)+
     xlab("log2fold") +
     ylab("-log10(adj. p.value)") +
     ggtitle(title) +
-    theme(plot.title = element_text(hjust = 0.5, face="bold.italic", size=14),
+    theme(plot.title = element_text(hjust = 0.4, face="bold.italic", size=14),
           axis.title.x = element_text(size=10),
           axis.title.y = element_text(size=10),
           axis.text.x = element_text(face="bold",size=10),
-          axis.text.y = element_text(face="bold", size=10), legend.position = "none")
-  
+          axis.text.y = element_text(face="bold", size=10), 
+          legend.position = "right")+
+   scale_color_manual(values=c("downregulated" = "blue", "non_signif." = "grey", "upregulated" = "red"))
+    
 }
 
-volplot(logt159, logt159$TvsN, logt159$p_adjust_T_N, "title")
+
+#Addig labels
+
+#tissue 159
+# add column of for expression
+logt159$diffexpres <- "non_signif."
+# if log2Foldchange > 1 and pvalue < 0.05, set as "upregulated"
+logt159$diffexpres[logt159$ORFvsIgG > 1 & logt159$p_adjust_ORF_IgG < 0.05] <- "upregulated"
+# if log2Foldchange < -1 and pvalue < 0.05, set as "downregulated"
+logt159$diffexpres[logt159$ORFvsIgG < -1 & logt159$p_adjust_ORF_IgG < 0.05] <- "downregulated"
+
+#same thing with the rest of data
+
+logt159$diffexpres2 <- "non_signif."
+logt159$diffexpres2[logt159$TvsN > 1 & logt159$p_adjust_T_N < 0.05] <- "upregulated"
+logt159$diffexpres2[logt159$TvsN < -1 & logt159$p_adjust_T_N < 0.05] <- "downregulated"
 
 
+#tissue 144
+logt144$diffexpres <- "non_signif."
+logt144$diffexpres[logt144$cond_9_10 > 1 & logt144$p_adjust_9_10 < 0.05] <- "upregulated"
+logt144$diffexpres[logt144$cond_9_10 < -1 & logt144$p_adjust_9_10 < 0.05] <- "downregulated"
+
+logt144$diffexpres2 <- "non_signif."
+logt144$diffexpres2[logt144$cond_1_2 > 1 & logt144$p_adjust_1_2 < 0.05] <- "upregulated"
+logt144$diffexpres2[logt144$cond_1_2 < -1 & logt144$p_adjust_1_2 < 0.05] <- "downregulated"
 
 
+#tissue 163
+logt163$diffexpres <- "non_signif."
+logt163$diffexpres[logt163$ORFvsIgG > 1 & logt163$p_adjust_ORF_IgG < 0.05] <- "upregulated"
+logt163$diffexpres[logt163$ORFvsIgG < -1 & logt163$p_adjust_ORF_IgG < 0.05] <- "downregulated"
 
-ggplot(data=logt159, aes(x=TvsN, y=-log10(p_adjust_T_N))) +
-  theme_bw()+
-  geom_point() + 
-  geom_vline(xintercept=c(-1, 1), col="red") +
-  geom_hline(yintercept=-log10(0.05), col="red")+
-  ylim(-1,4)
-
-
-ggplot(data=logt144, aes(x=p_val9_10, y=-log10(p_adjust_9_10))) +
-  theme_bw()+
-  geom_point(size = 1) + 
-  geom_vline(xintercept=c(-1, 1), col="blue") +
-  geom_hline(yintercept=-log10(0.05), col="green")+
-  ylim(-1,4)+
-  xlim(-7,10)
+logt163$diffexpres2 <- "non_signif."
+logt163$diffexpres2[logt163$TvsN > 1 & logt163$p_adjust_T_N < 0.05] <- "upregulated"
+logt163$diffexpres2[logt163$TvsN < -1 & logt163$p_adjust_T_N < 0.05] <- "downregulated"
 
 
-ggplot(data=logt159, aes(x=ORFvsIgG, y=-log10(p_adjust_ORF_IgG))) +
-  theme_bw()+
-  geom_point(size = 1) + 
-  geom_vline(xintercept=c(-1, 1), col="blue") +
-  geom_hline(yintercept=-log10(0.05), col="green")+
-  ylim(-1,4)+
-  xlim(-7,10)
+#Volcano plots
 
-ggplot(data=logt163, aes(x=ORFvsIgG, y=-log10(p_adjust_ORF_IgG))) +
-  theme_bw()+
-  geom_point(size = 1) + 
-  geom_vline(xintercept=c(-1, 1), col="blue") +
-  geom_hline(yintercept=-log10(0.05), col="green")+
-  ylim(-1,4)+
-  xlim(-7,10)
+volplot(logt159, logt159$ORFvsIgG, logt159$p_adjust_ORF_IgG, "tissue 159 ORF vs IgG", logt159$diffexpres)
+volplot(logt159, logt159$TvsN, logt159$p_adjust_T_N, "tissue 159 Tumor vs Normal (ORF)", logt159$diffexpres2)
 
+volplot(logt163, logt163$ORFvsIgG, logt163$p_adjust_ORF_IgG, "tissue 163 ORF vs IgG", logt163$diffexpres)
+volplot(logt163, logt163$TvsN, logt163$p_adjust_T_N, "tissue 163 Tumor vs Normal (ORF)", logt163$diffexpres2)
 
+volplot(logt144, logt144$cond_9_10, logt144$p_adjust_9_10, "tissue 144 ORF vs IgG (cond 9vs10)", logt144$diffexpres)
+volplot(logt144, logt144$cond_1_2, logt144$p_adjust_1_2, "tissue 144 ORF vs IgG (cond 1vs2)", logt144$diffexpres2)
 
 
 
